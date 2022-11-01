f00ec2619b6a65c18134286dd4413d4bccdb5891
Dominic Petriella <dpetriella@magicwrighter.com>
2022-11-01 16:00:31 -0400

    added skills to drawer, starting on edit dialogs

---
+++ lib/global_components/action_dialogs/simple_changeto.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SimpleChangeTo extends StatelessWidget {
+  const SimpleChangeTo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
